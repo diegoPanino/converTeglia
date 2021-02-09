4b2adcafba5a7b94af95729b883eb773605106a9
diegoPanino <tieso.diego@gmail.com>
2021-02-09 20:03:34 +0100

    Running on Win10

--- react-native.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	project: {
-		ios:{},
-		android: {},
-	},
-	assets: ['./assets/fonts/Kalam-Light','./src/assets/fonts/IndieFlower-Regular','./assets/fonts/Neucha-Regular.ttf','./assets/fonts/'],
-};
